c3948c4488cdc21ead4cf977ff8653f3cc068328
vvithurshan <vvithurshan@gmail.com>
2026-07-08 00:26:32 -0600

    Bug fixed with test_operations

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,4 @@
-from src.math_operations import add, substract, multiply, divide
+from src.math_operations import add, sub, mul, div
 
 def test_add():
     assert add(1, 2) == 3
@@ -14,44 +14,56 @@ def test_add():
     assert add(1, 0) == 1
     assert add(0, 1) == 1
 
-def test_substract():
-    assert substract(1, 2) == -1
-    assert substract(-1, -2) == 1
-    assert substract(-1, 1) == -2
-    assert substract(0, 0) == 0
-    assert substract(1, 0) == 1
-    assert substract(0, 1) == -1
-    assert substract(1, 2) == -1
-    assert substract(-1, -2) == 1
-    assert substract(-1, 1) == -2
-    assert substract(0, 0) == 0
-    assert substract(1, 0) == 1
-    assert substract(0, 1) == -1
+def test_sub():
+    assert sub(1, 2) == -1
+    assert sub(-1, -2) == 1
+    assert sub(-1, 1) == -2
+    assert sub(0, 0) == 0
+    assert sub(1, 0) == 1
+    assert sub(0, 1) == -1
+    assert sub(1, 2) == -1
+    assert sub(-1, -2) == 1
+    assert sub(-1, 1) == -2
+    assert sub(0, 0) == 0
+    assert sub(1, 0) == 1
+    assert sub(0, 1) == -1
+    assert sub(1, 2) == -1
+    assert sub(-1, -2) == 1
+    assert sub(-1, 1) == -2
+    assert sub(0, 0) == 0
+    assert sub(1, 0) == 1
+    assert sub(0, 1) == -1
 
 def test_multiply():
-    assert multiply(1, 2) == 2
-    assert multiply(-1, -2) == 2
-    assert multiply(-1, 1) == -1
-    assert multiply(0, 0) == 0
-    assert multiply(1, 0) == 0
-    assert multiply(0, 1) == 0
-    assert multiply(1, 2) == 2
-    assert multiply(-1, -2) == 2
-    assert multiply(-1, 1) == -1
-    assert multiply(0, 0) == 0
-    assert multiply(1, 0) == 0
-    assert multiply(0, 1) == 0
+    assert mul(1, 2) == 2
+    assert mul(-1, -2) == 2
+    assert mul(-1, 1) == -1
+    assert mul(0, 0) == 0
+    assert mul(1, 0) == 0
+    assert mul(0, 1) == 0
+    assert mul(1, 2) == 2
+    assert mul(-1, -2) == 2
+    assert mul(-1, 1) == -1
+    assert mul(0, 0) == 0
+    assert mul(1, 0) == 0
+    assert mul(0, 1) == 0
+    assert mul(1, 2) == 2
+    assert mul(-1, -2) == 2
+    assert mul(-1, 1) == -1
+    assert mul(0, 0) == 0
+    assert mul(1, 0) == 0
+    assert mul(0, 1) == 0
 
 def test_divide():
-    assert divide(1, 2) == 0.5
-    assert divide(-1, -2) == 0.5
-    assert divide(-1, 1) == -1
-    assert divide(0, 0) == 0
-    assert divide(1, 0) == 0
-    assert divide(0, 1) == 0
-    assert divide(1, 2) == 0.5
-    assert divide(-1, -2) == 0.5
-    assert divide(-1, 1) == -1
-    assert divide(0, 0) == 0
-    assert divide(1, 0) == 0
-    assert divide(0, 1) == 0
+    assert div(1, 2) == 0.5
+    assert div(-1, -2) == 0.5
+    assert div(-1, 1) == -1
+    assert div(0, 0) == 0
+    assert div(1, 0) == 0
+    assert div(0, 1) == 0
+    assert div(1, 2) == 0.5
+    assert div(-1, -2) == 0.5
+    assert div(-1, 1) == -1
+    assert div(0, 0) == 0
+    assert div(1, 0) == 0
+    assert div(0, 1) == 0
